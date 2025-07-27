c01b0b9ad1fa03225d9a303230c940995acf9ce0
jYoung706 <wnsdud9476@gmail.com>
2025-07-27 22:52:49 +0900

    주석 제거

--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -3,17 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 
-// @Injectable()
-// export class UsersRepository extends Repository<User> {
-//   constructor(dataSource: DataSource) {
-//     super(User, dataSource.createEntityManager());
-//   }
-
-//   async findAll() {
-//     return this.findAll();
-//   }
-// }
-
 @Injectable()
 export class UsersRepository {
   constructor(
